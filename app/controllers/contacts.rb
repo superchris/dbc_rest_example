9917eff8f6fcb676493d84fcd092207ff7eef4b3
Chris Nelson <chris@gaslightsoftware.com>
2014-07-14 17:31:25 -0400

    create contact sans redirect

--- app/controllers/contacts.rb
+++ app/controllers/contacts.rb
@@ -12,3 +12,8 @@ get "/contacts/:id" do
   @contact = Contact.find(params[:id])
   erb :"contacts/show"
 end
+
+post "/contacts" do
+  @contact = Contact.create(params[:contact])
+  erb :"contacts/show"
+end
