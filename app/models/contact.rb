da2efdc8f437bc3f555f209189a1b406bb2811c7
Chris Nelson <chris@gaslightsoftware.com>
2014-07-15 20:59:48 -0400

    some scopes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,12 @@
 class Contact < ActiveRecord::Base
   has_many :addresses, as: :addressable
   belongs_to :company
+
+  def self.only_gary
+    where(name: "Gary")
+  end
+
+  def self.at_gmail
+    where("email like '%gmail%'")
+  end
 end
