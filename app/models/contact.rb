cbbf9c1b89982f82e3d9299b1ba33a68d3bbedb8
Chris Nelson <chris@gaslightsoftware.com>
2014-07-28 22:00:54 -0400

    remember the bosses name

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,13 +2,13 @@ class Contact < ActiveRecord::Base
 
   self.table_name = :dbasrule_contacts
   self.primary_key = :not_id
-  
+
   has_many :addresses, as: :addressable
   belongs_to :company, foreign_key: "foodly_id"
 
-  belongs_to :boss, class_name: "Contact"
+  belongs_to :boss, class_name: "Contact", inverse_of: :peons
 
-  has_many :peons, class_name: "Contact", foreign_key: "boss_id"
+  has_many :peons, class_name: "Contact", inverse_of: :boss, foreign_key: "boss_id"
 
   def self.only_gary
     where(name: "Gary")
