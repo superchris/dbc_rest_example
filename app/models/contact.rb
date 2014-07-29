d0be62807529ba2ffd107dfa69f3173808ed5f94
Chris Nelson <chris@gaslightsoftware.com>
2014-07-28 21:52:43 -0400

    contact unborked

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,15 @@
 class Contact < ActiveRecord::Base
+
+  self.table_name = :dbasrule_contacts
+  self.primary_key = :not_id
+  
   has_many :addresses, as: :addressable
   belongs_to :company, foreign_key: "foodly_id"
 
   belongs_to :boss, class_name: "Contact"
 
   has_many :peons, class_name: "Contact", foreign_key: "boss_id"
-  
+
   def self.only_gary
     where(name: "Gary")
   end
